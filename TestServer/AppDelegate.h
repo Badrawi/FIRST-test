d3468dfcaaddbc13fd59fda34952f8cccda70a3f
Tryvin <tryvin@server.local>
2014-08-26 18:34:02 +0300

    Initial Commit

---
+++ TestServer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestServer
+//
+//  Created by Tryvin on 8/26/14.
+//  Copyright (c) 2014 Tryvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestServer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestServer
+//
+//  Created by Tryvin on 8/26/14.
+//  Copyright (c) 2014 Tryvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
